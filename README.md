6081f8bb7b207fd40aa0e275ad092040045710ae
Rickard31 <vitalka.marchenko@gmail.com>
2018-02-12 23:21:09 +0200

    Created Java projekt

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CloudIntro_Marchenko_Practice3
+
+This is Third Practice Task for Introduction to CLoud Technologies 
+by Marchenko Vitaliy
---
+++ src/SomeInterface.java
@@ -0,0 +1,7 @@
+
+public interface SomeInterface {
+	
+	String funnyJoke = "проаналізувати виконання арифметичних операцій";
+	void tellFunnyJoke();
+
+}
